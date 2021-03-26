174c04413a82236081963cb44396d4b994131d44
Ryan Mottola <rmottola68@gmail.com>
2021-03-26 04:50:46 -0400

    added npm's for bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <button className={'btn btn-outline-primary'}>poo poo pee pee</button>
-    </div>
-  );
+    <>
+ 
+     <div className={'navbar bg-primary border-bottom justify-content-center'}>
+       <h1 className={'text-light'}>Paper Trader</h1>
+       <button type={'button'} className={'btn btn-outline-primary'}>blue button</button>
+     </div>
+     
+     <div className={'container-fluid '}>
+       <div className={'row inline-flex'}>
+       <button type={'button'} className={'btn btn-outline-primary'}>blue button</button>
+        
+         
+       </div>
+     </div>
+ 
+     
+     
+ 
+ 
+    </>
+   );
 }
 
 export default App;
